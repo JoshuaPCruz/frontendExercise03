5295197f2127b23ee39d6c2e8b2637f6bcac5101
Osvaldo Arzate <osvarz.new@gmail.com>
2020-06-11 23:23:38 -0500

    Se termina seccion de articlos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('click', event);
 button = document.getElementsByClassName('btn');
 function event(e) {
   let color;
+  let textColor;
   const click = e.target;
   const btn = click;
   const btnClass = click.classList.value;
@@ -9,15 +10,18 @@ function event(e) {
     let aleatorio = parseInt(Math.random() * (max - min + 1)) + min;
     if (aleatorio === 2) {
       color = 'green';
+      textColor = 'white';
     }
     if (aleatorio === 3) {
       color = 'red';
+      textColor = 'white';
     }
     if (aleatorio === 4) {
       color = 'yellow';
     }
     if (aleatorio === 5) {
       color = 'blue';
+      textColor = 'white';
     }
     if (aleatorio === 6) {
       color = 'orange'
@@ -26,7 +30,8 @@ function event(e) {
   }
   random(0, 6);
   if (btnClass === 'btn') {
-    btn.classList.add(`${color}`);
+    btn.style.color = `${textColor}`;
+    btn.style.background = `${color}`;
   }
 }
 
